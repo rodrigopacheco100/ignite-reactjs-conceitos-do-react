6a2ec167ec4d15accb38c5b908206f46df3aa4d3
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2022-05-02 20:56:41 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,24 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+
+    setTasks(tasks=> [...tasks, { id: Math.random(),title: newTaskTitle, isComplete: false}])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks=> tasks.map(task => {
+      if(task.id === id) {
+        return {...task, isComplete: true}
+      }
+      return task
+    }))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks=> tasks.filter(task => task.id !== id))
   }
 
   return (
